e4280cacf59870ce0bafcbc34bc4da63c9c82d47
73trashpanda <80885133+73trashpanda@users.noreply.github.com>
2022-05-30 18:45:36 +0200

    Create brew.sh

---
+++ scripts/install/brew.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Inspiration from CoreyMSchafer/dotfiles on GitHub
+
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS Application
+brew cask install google-chrome
+brew cask install firefox
+brew cask install virtualbox
+brew cask install sourcetree
+brew cask install spotify
+brew cask install discord
+brew cask install gimp
+brew cask install vlc
+brew cask install onedrive
+brew cask install microsoft-auto-update
+brew cask install microsoft-office
+brew cask install gh
+brew cask install rich-cli
